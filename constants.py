f756c51a3269cc9029043e454d319829b9a3897b
Shital Rindhe <Shital.Rindhe@Xoriant.com>
2020-12-11 14:20:04 +0530

    deleting old code

--- constants.py
+++
@@ -1,7 +0,0 @@
-HEADERS = {'Content-type': 'application/json', 'Accept': 'application/json'}
-
-# Health URL
-HEALTH_URL = "http://<server_name>/toolqueue/actuator/health"
-CONFIG_FILE = './../runner_config.ini'
-SCRIPT = '../remove_data.bat'
-URL = 'http://<server_name>/'
--- remove_data.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Clearing Data
-set execution_path=D:\common\execution\
-set db_id=%1
-echo %execution_path%%db_id%
-RD /S /Q "%execution_path%%db_id%"
-echo removed directory %execution_path%%db_id%
-echo %ERRORLEVEL%
-exit %ERRORLEVEL%
