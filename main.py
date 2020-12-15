7bb96ca70151c49867430d8b470f083d25c21a8b
Shital Rindhe <Shital.Rindhe@Xoriant.com>
2020-12-15 14:39:38 +0530

    add comment

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # GET method
 @app.route('/')
 def func():
     return 'Hey there, this is first python program'
+
 
 # Main function
 if __name__ == '__main__':
