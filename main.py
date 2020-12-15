213308d2c587b662df73ca1127b6386e7ee144a5
Shital Rindhe <Shital.Rindhe@Xoriant.com>
2020-12-15 14:33:34 +0530

    added comments

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# GET method
 @app.route('/')
 def func():
     return 'Hey there, this is first python program'
 
-
+# Main function
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=False)
